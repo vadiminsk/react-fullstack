3003020eedc3960bf9051d8978ff84a3ac450084
vadiminsk <vadim@graphicalliance.co.uk>
2021-02-17 22:46:22 +0300

    add events

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+  inputChangeHandler(e) {
+    console.log(e.target.value);
+  }
+
   render() {
     return (
       <header>
         <div className='logo'>Logo</div>
+        <input onChange={this.inputChangeHandler} />
       </header>
     );
   }
