5e03f7924840fd481cc2ef9a911260008f9f2350
vadiminsk <vadim@graphicalliance.co.uk>
2021-02-22 22:37:23 +0300

    add style condition according to state

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,29 +3,22 @@ import React, { Component } from 'react';
 class Header extends Component {
   state = {
     value: '',
-    count: 0,
+    active: false,
   };
 
   inputChangeHandler = (e) => {
+    const inputState = e.target.value ? true : false;
     this.setState({
+      active: inputState,
       value: e.target.value,
     });
   };
 
-  addOne() {
-    this.setState((state, props) => ({
-      count: state.count + 1,
-    }));
-  }
-
   render() {
     return (
-      <header>
+      <header style={{ background: `${this.state.active ? 'red' : 'blue'} ` }}>
         <div className='logo'>Logo</div>
         <input onChange={this.inputChangeHandler} />
-        <div>{this.state.value}</div>
-        <div>{this.state.count}</div>
-        <button onClick={() => this.addOne()}>Add one</button>
       </header>
     );
   }
